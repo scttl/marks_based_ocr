57a7b9522b25c3a0cfc70e6fa505c1f209b3e94b
scottl <scottl>
2006-06-19 21:52:12 +0000

    small change to use length instead of size.

--- line/get_lines.m
+++ line/get_lines.m
@@ -21,11 +21,14 @@ function Pos = get_lines(Clust, Comps, num)
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: get_lines.m,v 1.2 2006-06-12 20:59:40 scottl Exp $
+% $Id: get_lines.m,v 1.3 2006-06-19 21:52:12 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: get_lines.m,v $
-% Revision 1.2  2006-06-12 20:59:40  scottl
+% Revision 1.3  2006-06-19 21:52:12  scottl
+% small change to use length instead of size.
+%
+% Revision 1.2  2006/06/12 20:59:40  scottl
 % use cell array for comps, allow one to specify the number of lines returned
 %
 % Revision 1.1  2006/06/03 20:56:01  scottl
@@ -36,17 +39,17 @@ function Pos = get_lines(Clust, Comps, num)
 % LOCAL VARIABLES %
 %%%%%%%%%%%%%%%%%%%
 max_num = inf;
-num_pages = size(Comps,1);
+num_pages = length(Comps);
 Pos = cell(num_pages,1);
-num_clust = size(Clust,1);
+num_clust = length(Clust);
 
-display_images = true;
+display_images = false;
 save_images = false;
 %RGB colour values used to display images if set to true above
 out_col = reshape([255,0,0],1,1,3);  % this is red
 
 %the filename and file type to save the image (display_images must be true)
-img_prefix = 'images/aa01_lines';
+img_prefix = 'results/nips5_lines';
 img_format = 'png';
 
 
