552e6bda77a1a79e26623dfc71a5a9b9985f7ded
scottl <scottl>
2006-07-05 04:36:35 +0000

    small updates to comments.

--- marks_ocr_init.m
+++ marks_ocr_init.m
@@ -1,26 +1,34 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-%% FILE: startup.m
+%% FILE: marks_ocr_init.m
 %%
 %% DESCRIPTION: Sets initial Matlab defaults for use in the marks-based 
 %%              OCR project
 %%
+%% NOTE: Be sure to set the global MOCR_PATH variable in your startup.m file
+%%       to point at the path where this file resides, then executre this
+%%       file.
 %% CVS:
-%% $Id: marks_ocr_init.m,v 1.2 2006-06-05 16:20:34 scottl Exp $
+%% $Id: marks_ocr_init.m,v 1.3 2006-07-05 04:36:35 scottl Exp $
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fprintf('loading matlab defaults file\n');
+fprintf('Initializing Marks-Based OCR\n');
 ver = version;
 if str2num(ver(1)) < 7
     warning('Using Matlab version < 7, some functions may not work correctly');
 end
+
 more off;  %turn off pagination
 whitebg;   %inverse the figure backgrounds
 close(gcf);
 
 %add the necessary paths
-addpath([pwd, '/cluster']);
-addpath([pwd, '/util']);
-addpath([pwd, '/display']);
-addpath([pwd, '/ocr']);
-addpath([pwd, '/lang_model']);
+global MOCR_PATH;
+if isempty(MOCR_PATH)
+    error('Must set global MOCR_PATH to the install directory.  See README');
+end
+addpath([MOCR_PATH, '/cluster']);
+addpath([MOCR_PATH, '/util']);
+addpath([MOCR_PATH, '/display']);
+addpath([MOCR_PATH, '/ocr']);
+addpath([MOCR_PATH, '/lang_model']);
