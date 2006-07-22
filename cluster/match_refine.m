ea6b746d42d4d127ed4599f9b972168e548a7dc2
scottl <scottl>
2006-07-22 04:10:30 +0000

    cleaned up comments, implemented alternate distance metrics, modified add_and_reaverage

--- cluster/match_refine.m
+++ cluster/match_refine.m
@@ -1,8 +1,7 @@
 function [Clust, Comps] = match_refine(Clust, Comps, dm, thr)
 % match_refine  Attempt to match near-identical Clusters
 %
-%   [Clust, Comps, chg_list] = match_refine(Clust, Comps, refine_list, ...
-%                                           dist_metric, thresh)
+%   [Clust, Comps, chg_list] = match_refine(Clust, Comps, [dist_metric, thresh])
 %
 %   Clust should be a struct containing several fields specifying which 
 %   components belong to each cluster, and their averages etc.  See 
@@ -17,29 +16,33 @@ function [Clust, Comps] = match_refine(Clust, Comps, dm, thr)
 %   after convolving the matrices to find maximal overlapping point), 
 %   'hausdorff' to use Hausdorff distance, or 'ham' for Hamming distance
 %
-%   thresh is optional and if specified, determines the normalized maximal 
-%   Euclidian distance allowable for two cluster averages to be considered 
-%   matching.  If not specified, it defaults to .009
+%   thresh is optional and if specified, determines the maximal 
+%   distance allowable for two cluster averages to be considered 
+%   matching.  If not specified, it defaults to .009 (suitable for use with
+%   'euc' as a distance metric).
 %
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: match_refine.m,v 1.2 2006-07-05 01:16:58 scottl Exp $
+% $Id: match_refine.m,v 1.3 2006-07-22 04:10:30 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: match_refine.m,v $
-% Revision 1.2  2006-07-05 01:16:58  scottl
+% Revision 1.3  2006-07-22 04:10:30  scottl
+% cleaned up comments, implemented alternate distance metrics, modified
+% add_and_reaverage
+%
+% Revision 1.2  2006/07/05 01:16:58  scottl
 % rewritten based on new Cluster and Component structures.
 %
 % Revision 1.1  2006/06/03 20:55:48  scottl
 % Initial check-in.
-%
-%
+
 
 % LOCAL VARS %
 %%%%%%%%%%%%%%
 dist_metric = 'euc';              %default distance metric
-distance_thresh = 0.009;          %default distance theshold
+distance_thresh = 0.009;          %default distance threshold
 
 display_images = false;  %set this to true to display matches as they are found
 
@@ -61,19 +64,18 @@ while ~isempty(rr)
     fprintf('                                                         \r');
     fprintf('cluster: %d  -- ', rr);
 
-    D = euc_dist(Clust.avg{rr}, Clust.avg, Clust.norm_sq(rr), Clust.norm_sq);
-%        elseif strcmp(dist_metric, 'conv_euc')
-%            [match_found, Mr, Mi, d] = conv_euc_match(Clust(r).avg, ...
-%                                   Clust(i).avg, distance_thresh);
-%        elseif strcmp(dist_metric, 'hausdorff')
-%            [match_found, Mr, Mi, d] = hausdorff_match(Clust(r).avg, ...
-%                                   Clust(i).avg, distance_thresh);
-%        elseif strcmp(dist_metric, 'ham')
-%            [match_found, Mr, Mi, d] = ham_match(Clust(r).avg, ...
-%                                   Clust(i).avg, distance_thresh);
-%        else
-%            error('incorrect distance metric specified!');
-%        end
+    if strcmp(dist_metric, 'euc')
+        D = euc_dist(Clust.avg{rr},Clust.avg,Clust.norm_sq(rr),Clust.norm_sq);
+    elseif strcmp(dist_metric, 'hausdorff')
+        D = hausdorff_dist(Clust.avg{rr}, Clust.avg);
+    elseif strcmp(dist_metric, 'ham')
+        D = ham_dist(Clust.avg{rr}, Clust.avg);
+    elseif strcmp(dist_metric, 'conv_euc')
+        D = conv_euc_dist(Clust.avg{rr},Clust.avg,Clust.norm_sq(rr), ...
+            Clust.norm_sq);
+    else
+        error('incorrect distance metric specified!');
+    end
     match_idcs = find(D <= distance_thresh);
     match_idcs = match_idcs(match_idcs ~= rr);
     num_match = length(match_idcs);
@@ -94,7 +96,7 @@ while ~isempty(rr)
             pause(.5);
         end
         %merge the clusters together, note that this will re-order the clusters
-        [Clust, Comps] = add_and_reaverage(Clust, Comps, rr, match_idcs);
+        [Clust, Comps] = add_and_reaverage(Clust, Comps, rr, match_idcs, true);
     else
         Clust.refined(rr) = true;
         fprintf('no match\r');
