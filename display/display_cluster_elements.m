96851451f0b1591135c1b75c615a633cab2605bd
scottl <scottl>
2006-08-24 21:37:37 +0000

    added ability to specify particular component indices to be displayed.

--- display/display_cluster_elements.m
+++ display/display_cluster_elements.m
@@ -1,7 +1,7 @@
-function display_cluster_elements(Clust, Comps, idx, num)
+function display_cluster_elements(Clust, Comps, idx, num, comp_idx)
 % DISPLAY_CLUSTER_ELEMENTS   Display the elements of clusters
 %
-%   DISPLAY_CLUSTER_ELEMENTS(Clust, Comps, idx, [num])
+%   DISPLAY_CLUSTER_ELEMENTS(Clust, Comps, idx, [num, comp_idx])
 %
 %   Clust and Comps should be as defined in cluster_comps.m
 %
@@ -12,15 +12,26 @@ function display_cluster_elements(Clust, Comps, idx, num)
 %   num is optional and if specified determines the (maximum) number of elements
 %   that should be drawn for each cluster.  If not specified it defaults to 50.
 %   The elements are randomly sampled from each cluster (containing > 50 
-%   elements)
+%   elements, unless comp_idx is passed)
+%
+%   comp_idx is optional and if specified gives an explicit ordering of the
+%   components to display.  If not passed, some random subsample of components
+%   is used.  It should be passed as a column vector with elements belonging to
+%   the same Cluster, listed in the order they would like to be drawn.  Note
+%   that when passed, even though num is required, it will be ignored, and the
+%   number of components to draw is calculated based on the list passed.
+%
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: display_cluster_elements.m,v 1.3 2006-08-07 21:19:23 scottl Exp $
+% $Id: display_cluster_elements.m,v 1.4 2006-08-24 21:37:37 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: display_cluster_elements.m,v $
-% Revision 1.3  2006-08-07 21:19:23  scottl
+% Revision 1.4  2006-08-24 21:37:37  scottl
+% added ability to specify particular component indices to be displayed.
+%
+% Revision 1.3  2006/08/07 21:19:23  scottl
 % remove dependence on imview
 %
 % Revision 1.2  2006/07/21 20:27:09  scottl
@@ -35,6 +46,7 @@ function display_cluster_elements(Clust, Comps, idx, num)
 num_to_draw = 50;
 row_pix_border = 10;
 col_pix_border = 3;
+use_random = true;  %by default use a random sample of elements from the cluster
 
 %set save_elements to true to write the elements image to disk based on the 
 %params below it
@@ -46,24 +58,33 @@ img_format = 'png';
 % CODE START %
 %%%%%%%%%%%%%%
 
-if nargin < 3 || nargin > 4
+if nargin < 3 || nargin > 5
     error('incorrect number of arguments specified!');
-elseif nargin == 4
+elseif nargin >= 4
     num_to_draw = num;
+    if nargin == 5
+        use_random = false;
+    end
 end
 
-%collect the random Component indices of each cluster
-comp_list = [];
 num_clusts = length(idx);
-lrg_idx = find(Clust.num_comps(idx) > num_to_draw);
-lrg_idx = idx(lrg_idx);
-sm_idx = setdiff(idx, lrg_idx);
-for ii=lrg_idx
-    this_comps = Clust.comps{ii}(ceil(rand(num_to_draw,1) .* ...
-                                 Clust.num_comps(ii)));
-    comp_list = [comp_list; this_comps];
+if use_random
+    %collect the random Component indices of each cluster
+    comp_list = [];
+    lrg_idx = find(Clust.num_comps(idx) > num_to_draw);
+    lrg_idx = idx(lrg_idx);
+    sm_idx = setdiff(idx, lrg_idx);
+    for ii=lrg_idx
+        this_comps = Clust.comps{ii}(ceil(rand(num_to_draw,1) .* ...
+                         Clust.num_comps(ii)));
+        comp_list = [comp_list; this_comps];
+    end
+    comp_list = [comp_list; cell2mat(Clust.comps(sm_idx))];
+else
+    %use the component indices passed
+    comp_list = comp_idx;
+    [num_to_draw,num_to_draw] = mode(Comps.clust(comp_idx));
 end
-comp_list = [comp_list; cell2mat(Clust.comps(sm_idx))];
 
 %create a cell arrray to hold the component images
 M = cell(num_clusts,num_to_draw);
