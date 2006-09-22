2c00c2443a7235f154aae0b8bd920c4a345f3e21
scottl <scottl>
2006-09-22 18:00:43 +0000

    updated MSGID for warning message.

--- util/process_optional_args.m
+++ util/process_optional_args.m
@@ -8,10 +8,13 @@ function process_optional_args(varargin)
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: process_optional_args.m,v 1.1 2006-09-20 21:58:30 scottl Exp $
+% $Id: process_optional_args.m,v 1.2 2006-09-22 18:00:43 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: process_optional_args.m,v $
+% Revision 1.2  2006-09-22 18:00:43  scottl
+% updated MSGID for warning message.
+%
 % Revision 1.1  2006-09-20 21:58:30  scottl
 % initial checkin
 %
@@ -28,5 +31,6 @@ if rem(arglen, 2) ~= 0
 end
 for ii=1:2:arglen-1
     assignin('caller', varargin{ii}, varargin{ii+1});
-    warning('MBOCR', 'overriding value for variable: %s', varargin{ii});
+    warning('MBOCR:override', 'overriding value for variable: %s', ...
+            varargin{ii});
 end
