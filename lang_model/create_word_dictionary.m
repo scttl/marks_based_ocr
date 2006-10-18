e3114558013467a9d2265894809b6e2a8b360352
scottl <scottl>
2006-10-18 16:03:15 +0000

    add first character of line counts (for use in HMM's)

--- lang_model/create_word_dictionary.m
+++ lang_model/create_word_dictionary.m
@@ -15,11 +15,14 @@ function D = create_word_dictionary(Files)
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: create_word_dictionary.m,v 1.1 2006-06-10 21:01:40 scottl Exp $
+% $Id: create_word_dictionary.m,v 1.2 2006-10-18 16:03:15 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: create_word_dictionary.m,v $
-% Revision 1.1  2006-06-10 21:01:40  scottl
+% Revision 1.2  2006-10-18 16:03:15  scottl
+% add first character of line counts (for use in HMM's)
+%
+% Revision 1.1  2006/06/10 21:01:40  scottl
 % Initial revision.
 %
 
@@ -29,6 +32,8 @@ num_files = size(Files,1);
 D = {};
 W = {};
 C = [];
+
+eol = 10;  %encoding for the character corresponding to the newline character
 
 
 % CODE START %
@@ -68,13 +73,20 @@ fprintf('%.2fs: finished creating and counting chars\n', toc);
 
 num_chars = length(D.char);
 D.char_bigram = zeros(num_chars);
+D.first_count = zeros(num_chars, 1);
+
 pos = zeros(1, max(single(D.char)));
 for i = 1:num_chars
     pos(single(D.char(i))) = i;
 end
 
+D.first_count(pos(C(1))) = 1;  %the 1st character is the start of the 1st line
+
 for i = 2:length(C)
     D.char_bigram(pos(C(i-1)), pos(C(i))) = ...
     D.char_bigram(pos(C(i-1)), pos(C(i))) + 1;
+    if C(i-1) == eol
+        D.first_count(pos(C(i))) = D.first_count(pos(C(i))) + 1;
+    end
 end
-fprintf('%.2fs: finished creating character bigram matrix\n', toc);
+fprintf('%.2fs: finished creating character bigram and prior matrix\n', toc);
