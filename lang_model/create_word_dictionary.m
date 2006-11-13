b74ea5bef5e2e4da5e0331ee748c2c9cc9d9be5f
scottl <scottl>
2006-11-13 17:58:03 +0000

    added trigram counts.

--- lang_model/create_word_dictionary.m
+++ lang_model/create_word_dictionary.m
@@ -6,8 +6,8 @@ function D = create_word_dictionary(Files, varargin)
 %   Files should be a cell array (or single string), and give the full 
 %   path and name of the file(s) to be used in creating the dictionary.
 %
-%   D is a struct with fields: word, word_count, char, char_count, char_bigram
-%   and fist_count
+%   D is a struct with fields: word, word_count, char, char_count, char_bigram,
+%   char_trigram, first_count, and first_bg_count
 %
 %  NOTE: This requires matlab7 since it uses textscan, and assumes the text
 %  has been preprocessed to remove all unwanted (punctuation/numeric/control)
@@ -16,10 +16,13 @@ function D = create_word_dictionary(Files, varargin)
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: create_word_dictionary.m,v 1.3 2006-10-29 17:18:45 scottl Exp $
+% $Id: create_word_dictionary.m,v 1.4 2006-11-13 17:58:03 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: create_word_dictionary.m,v $
+% Revision 1.4  2006-11-13 17:58:03  scottl
+% added trigram counts.
+%
 % Revision 1.3  2006-10-29 17:18:45  scottl
 % updated argument handling, ability to strip unwanted characters, etc.
 %
@@ -70,7 +73,9 @@ fprintf('%.2fs: finished creating and counting chars\n', toc);
 
 num_chars = length(D.char);
 D.char_bigram = zeros(num_chars);
+D.char_trigram = zeros(num_chars, num_chars, num_chars);
 D.first_count = zeros(num_chars, 1);
+D.first_bg_count = zeros(num_chars);
 
 pos = zeros(1, max(single(D.char)));
 for ii=1:num_chars
@@ -78,18 +83,28 @@ for ii=1:num_chars
 end
 
 D.first_count(pos(C(1))) = 1;  %the 1st character is the start of the 1st line
+D.first_bg_count(pos(C(2)),pos(C(2))) = 1;  %the 1st 2 chars on 1st line
+D.char_bigram(pos(C(1)),pos(C(2))) = 1;  %count the 1st 2 chars separately
 
-for ii=2:length(C)
-    D.char_bigram(pos(C(ii-1)), pos(C(ii))) = ...
-    D.char_bigram(pos(C(ii-1)), pos(C(ii))) + 1;
-    if C(ii-1) == eol
-        D.first_count(pos(C(ii))) = D.first_count(pos(C(ii))) + 1;
+for ii=3:length(C)
+    prev_prev = pos(C(ii-2));
+    prev = pos(C(ii-1));
+    curr = pos(C(ii));
+    D.char_trigram(prev_prev,prev,curr) = ...
+    D.char_trigram(prev_prev,prev,curr) + 1;
+    D.char_bigram(prev,curr) = D.char_bigram(prev,curr) + 1;
+    if C(ii-2) == eol
+        D.first_bg_count(prev,curr) = D.first_bg_count(prev,curr) + 1;
+    elseif C(ii-1) == eol
+        D.first_count(curr) = D.first_count(curr) + 1;
     end
 end
-fprintf('%.2fs: finished creating character bigram and prior matrix\n', toc);
+fprintf('%.2fs: finished creating character n-gram matrices\n', toc);
 
 [D.char, idx] = setdiff(D.char, char(strip_chars)');
 D.char_count = D.char_count(idx);
 D.first_count = D.first_count(idx);
-D.char_bigram = D.char_bigram(idx, idx);
+D.first_bg_count = D.first_bg_count(idx,idx);
+D.char_bigram = D.char_bigram(idx,idx);
+D.char_trigram = D.char_trigram(idx,idx,idx);
 fprintf('%.2fs: finished stripping unwanted characters\n', toc);
