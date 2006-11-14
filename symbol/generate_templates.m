87c9aefd40de892795d2c8c4ec55a400ac19848a
scottl <scottl>
2006-11-14 22:51:14 +0000

    changed to a cell array of strings instead of chars.

--- symbol/generate_templates.m
+++ symbol/generate_templates.m
@@ -1,7 +1,7 @@
-function bitmaps = generate_templates(fontname, cn, varargin)
+function bitmaps = generate_templates(fontname, sn, varargin)
 % GENERATE_TEMPLATES  Create bitmaps of characters using the fontname passed
 %
-%   bitmaps = generate_templates(fontname, charnames, [var1, val1]...)
+%   bitmaps = generate_templates(fontname, symnames, [var1, val1]...)
 %   This procedure makes use of ImageMagick's convert utility to generate
 %   bitmap images of a particular font in a given size.
 %
@@ -9,15 +9,18 @@ function bitmaps = generate_templates(fontname, cn, varargin)
 %   If ImageMagick isn't present, or doesn't know about the fontname, an error
 %   is returned.
 %
-%   charnames should be a list of characters to generate bitmaps of.
+%   symnames should be a cell array of symbols to generate bitmaps of.
 %
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: generate_templates.m,v 1.4 2006-10-29 17:05:49 scottl Exp $
+% $Id: generate_templates.m,v 1.5 2006-11-14 22:51:14 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: generate_templates.m,v $
+% Revision 1.5  2006-11-14 22:51:14  scottl
+% changed to a cell array of strings instead of chars.
+%
 % Revision 1.4  2006-10-29 17:05:49  scottl
 % moved and renamed dofont.m to generate_templates.m.  Rewritten to make
 % use of imageMagick's convert utility.
@@ -43,31 +46,31 @@ elseif nargin > 2
     process_optional_args(varargin{:});
 end
 
-numchars=length(cn);
+numsyms=length(sn);
 
-for ii=1:numchars
-    fprintf(1,'Char %c (%d/%d)\n',cn(ii),ii,numchars);
+for ii=1:numsyms
+    fprintf(1,'Symbol %s (%d/%d)\n',sn{ii},ii,numsyms);
 
-    if strcmp(cn(ii), ' ')
+    if strcmp(sn{ii}, ' ')
         %space character.  Can't render a bitmap of this using imagemagick
         %so we'll just create one by hand, using ptsize as a loose guide
         dims = floor(ptsize/2);
         bitmaps{ii,:} = zeros(dims,dims);
         continue;
-    elseif strcmp(cn(ii), '''')
+    elseif strcmp(sn{ii}, '''')
         %since the character is a single quote, we must escape it differently
         %for the shell
         cmd = ['convert -trim -font ', fontname, ' -pointsize ', ...
                      num2str(ptsize), ' label:"\''" ', tmp_img];
-    elseif strcmp(cn(ii), '"')
+    elseif strcmp(sn{ii}, '"')  %same problem with double quotes
         cmd = ['convert -trim -font ', fontname, ' -pointsize ', ...
                      num2str(ptsize), ' label:''\"'' ', tmp_img];
-    elseif strcmp(cn(ii), '@')
+    elseif strcmp(sn{ii}, '@')
         cmd = ['convert -trim -font ', fontname, ' -pointsize ', ...
-                     num2str(ptsize), ' label:''\', cn(ii), ''' ', tmp_img];
+                     num2str(ptsize), ' label:''\', sn{ii}, ''' ', tmp_img];
     else
         cmd = ['convert -trim -font ', fontname, ' -pointsize ', ...
-                     num2str(ptsize), ' label:''', cn(ii), ''' ', tmp_img];
+                     num2str(ptsize), ' label:''', sn{ii}, ''' ', tmp_img];
     end
     [s,w] = unix(cmd);
 
@@ -80,7 +83,7 @@ for ii=1:numchars
 
     unix(['rm -f ' tmp_img]);
 
-    if strip_dots && (strcmp(cn(ii), 'i') || strcmp(cn(ii), 'j'))
+    if strip_dots && (strcmp(sn{ii}, 'i') || strcmp(sn{ii}, 'j'))
         %strip the dots from these lowercase letters to match what we do during
         %connected components finding
         [lbl_img, count] = bwlabel(bitmaps{ii,:});
