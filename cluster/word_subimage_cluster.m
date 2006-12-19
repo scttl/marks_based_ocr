bf46abd00513dd4ab0cd286f037b5e21413cba2f
scottl <scottl>
2006-12-19 21:43:25 +0000

    check in of partially implemented word subimage clustering.  Since this technique does not perform particularly well, I did not bother with the rest of the implementation.

--- cluster/word_subimage_cluster.m
+++ cluster/word_subimage_cluster.m
@@ -16,10 +16,15 @@ function clust = word_subimage_cluster(Lines, varargin)
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: word_subimage_cluster.m,v 1.1 2006-12-05 15:58:28 scottl Exp $
+% $Id: word_subimage_cluster.m,v 1.2 2006-12-19 21:43:25 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: word_subimage_cluster.m,v $
+% Revision 1.2  2006-12-19 21:43:25  scottl
+% check in of partially implemented word subimage clustering.  Since
+% this technique does not perform particularly well, I did not bother
+% with the rest of the implementation.
+%
 % Revision 1.1  2006-12-05 15:58:28  scottl
 % initial incomplete revision.
 %
@@ -36,7 +41,7 @@ split_thresh = 0.01;
 euc_match_thresh = 0.005;
 haus_match_thresh = 1.0;
 
-%each time we call match_refine etc.
+%prevent showing warning each time we call match_refine etc.
 override_display = 'OFF';   %other legal value is 'ON'
 
 %this determines how close to an edge a match must be to still be considerd a
@@ -50,6 +55,13 @@ min_area = 20;
 %how many pixels wide must an image be for it to be considered valid
 min_width = 7;
 
+%should we display the matches that we find to the screen?
+display_matches = false;
+
+%these variables can be given queue img and cluster imgs to use (instead of
+%estimating them from the Lines struct
+passed_queue = [];
+passed_clust = [];
 
 % CODE START %
 %%%%%%%%%%%%%%
@@ -63,55 +75,63 @@ elseif nargin > 1
 end
 prev_comp_warn = warning(override_display, 'MBOCR:noCompImg');
 prev_blank_warn = warning(override_display, 'MBOCR:blankImg');
+prev_over_warn = warning(override_display, 'MBOCR:override');
 
-%determine space width threshold @@TO DO@@
-space_width = 9;
-
-queue.sum = [];
-queue.img = cell(0);
-clust.sum = [];
-clust.img = cell(0);
-
-%break the lines into words
-curr_pg = Lines.pg(1);
-curr_img = ~imread(Lines.files{curr_pg});
-%@@for ii=1:Lines.num
-for ii=1:42
-    if Lines.pg(ii) ~= curr_pg
-        curr_pg = Lines.pg(ii);
-        curr_img = ~imread(Lines.files(curr_pg));
+if ~isempty(passed_queue) && ~isempty(passed_clust)
+    fprintf('using passed queue and cluster images\n');
+    queue.img = passed_queue;
+    clust.img = passed_clust;
+else
+    %determine space width threshold @@TO DO@@
+    space_width = 9;
+    
+    queue.sum = [];
+    queue.img = cell(0);
+    clust.sum = [];
+    clust.img = cell(0);
+    
+    %break the lines into words
+    curr_pg = Lines.pg(1);
+    curr_img = ~imread(Lines.files{curr_pg});
+    for ii=1:Lines.num
+        if Lines.pg(ii) ~= curr_pg
+            curr_pg = Lines.pg(ii);
+            curr_img = ~imread(Lines.files(curr_pg));
+        end
+        line_img = curr_img(Lines.pos(ii,2):Lines.pos(ii,4), ...
+                            Lines.pos(ii,1):Lines.pos(ii,3));
+    
+        queue.img = [queue.img; get_words(line_img, space_width)];
     end
-    line_img = curr_img(Lines.pos(ii,2):Lines.pos(ii,4), ...
-                        Lines.pos(ii,1):Lines.pos(ii,3));
-
-    queue.img = [queue.img; get_words(line_img, space_width)];
-end
-
-%match identical words (average them) @@TO DO@@
-queue.norm = zeros(length(queue.img),1);
-for ii=1:length(queue.img)
-    queue.norm(ii) = sum(queue.img{ii}(:));
-end
-idx = 1;
-fprintf('queue length: %d\n', length(queue.img));
-while (idx <= length(queue.img))
-    fprintf('item: %d ', idx);
-    curr_img = queue.img{idx};
-    curr_norm = queue.norm(idx);
-    d = euc_dist(curr_img, queue.img, curr_norm, queue.norm);
-    %d = ham_dist(curr_img, queue.img);
-    match = find(d <= euc_match_thresh);
-    match = setdiff(match, idx);  %index will always match with 0 distance
-    if ~isempty(match)
-        %subplot(1,2,1), imshow(curr_img);
-        %subplot(1,2,2), imshow(imgcell2mat(queue.img(match)));
-        %d(match)
-        %pause
-        queue.img = queue.img(setdiff(1:length(queue.img), match));
-        queue.norm = queue.norm(setdiff(1:length(queue.norm), match));
+    
+    %match identical words (average them) @@TO DO@@
+    queue.norm = zeros(length(queue.img),1);
+    for ii=1:length(queue.img)
+        queue.norm(ii) = sum(queue.img{ii}(:));
     end
-    fprintf(' %d matches\r', length(match));
-    idx = idx+1;
+    idx = 1;
+    fprintf('queue length: %d\n', length(queue.img));
+    while (idx <= length(queue.img))
+        fprintf('item: %d ', idx);
+        curr_img = queue.img{idx};
+        curr_norm = queue.norm(idx);
+        d = euc_dist(curr_img, queue.img, curr_norm, queue.norm);
+        %d = ham_dist(curr_img, queue.img);
+        match = find(d <= euc_match_thresh);
+        match = setdiff(match, idx);  %index will always match with 0 distance
+        if ~isempty(match)
+            if display_matches
+                subplot(1,2,1), imshow(curr_img);
+                subplot(1,2,2), imshow(imgcell2mat(queue.img(match)));
+                d(match)
+                pause(1);
+            end
+            queue.img = queue.img(setdiff(1:length(queue.img), match));
+            queue.norm = queue.norm(setdiff(1:length(queue.norm), match));
+        end
+        fprintf(' %d matches\r', length(match));
+        idx = idx+1;
+    end
 end
 fprintf('\nqueue length: %d\n', length(queue.img));
 
@@ -120,7 +140,7 @@ fprintf('\nqueue length: %d\n', length(queue.img));
 while ~isempty(queue.img)
     %subplot(1,2,1), imshow(imgcell2mat(queue.img));
     %subplot(1,2,2), imshow(imgcell2mat(clust.img));
-    %pause;
+    %pause(1);
     fprintf('queue length %d, cluster length %d\n', length(queue.img), ...
             length(clust.img));
     curr_img = queue.img{1};
@@ -134,6 +154,11 @@ while ~isempty(queue.img)
     if ~isempty(d) && d(1) <= haus_match_thresh
         %add this image to the cluster @@TO DO@@
         fprintf('image matches cluster %d\n', idx(1));
+        if display_matches
+            subplot(1,2,1), imshow(curr_img);
+            subplot(1,2,2), imshow(clust.img{idx(1)});
+            pause(1);
+        end
     else
         cus = size(curr_img);
         rem_idx = [];
@@ -155,6 +180,10 @@ while ~isempty(queue.img)
             end
             [t,l,score] = subimage_match(sub_img, img, 'thresh', split_thresh);
             if ~isempty(l)
+                if display_matches
+                    display_subimage_match(sub_img, img, t, l);
+                    pause(1);
+                end
                 r = l+sub_sz(2)-1;
                 if l(1) <= edge_tolerance
                     l = l(2:end);
@@ -254,6 +283,7 @@ end
 if strcmp(override_display, 'OFF')
     warning(prev_comp_warn.state, 'MBOCR:noCompImg');
     warning(prev_blank_warn.state, 'MBOCR:blankImg');
+    warning(prev_over_warn.state, 'MBOCR:override');
 end
 
 fprintf('%.2f: TOTAL NUMBER OF CLUSTERS FOUND: %d\n', toc, length(clust.img));
