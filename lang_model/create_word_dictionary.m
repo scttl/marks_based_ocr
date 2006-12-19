91c4bb589b58db97cd8d6e5e9abe1bcafbb652cc
scottl <scottl>
2006-12-19 21:38:19 +0000

    added character positional count information.

--- lang_model/create_word_dictionary.m
+++ lang_model/create_word_dictionary.m
@@ -6,20 +6,21 @@ function D = create_word_dictionary(Files, varargin)
 %   Files should be a cell array (or single string), and give the full 
 %   path and name of the file(s) to be used in creating the dictionary.
 %
-%   D is a struct with fields: word, word_count, char, char_count, char_bigram,
-%   char_trigram, first_count, and first_bg_count
+%   D is a struct with fields: word, word_count, char, char_count, pos_count,
+%   char_bigram, char_trigram, first_count, and first_bg_count
 %
-%  NOTE: This requires matlab7 since it uses textscan, and assumes the text
-%  has been preprocessed to remove all unwanted (punctuation/numeric/control)
-%  characters.
+%  NOTE: This requires matlab7 since it uses textscan
 %
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: create_word_dictionary.m,v 1.4 2006-11-13 17:58:03 scottl Exp $
+% $Id: create_word_dictionary.m,v 1.5 2006-12-19 21:38:19 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: create_word_dictionary.m,v $
+% Revision 1.5  2006-12-19 21:38:19  scottl
+% added character positional count information.
+%
 % Revision 1.4  2006-11-13 17:58:03  scottl
 % added trigram counts.
 %
@@ -42,14 +43,20 @@ C = [];
 
 eol = 10;  %encoding for the character corresponding to the newline character
 
-%list of character to disclude from out counts.  Typically these will be the
+%list of character to disclude from our counts.  Typically these will be the
 %non-printable chars though it can be customized
 strip_chars = [1:31,127];
+
+%when creating the positional counts, up to what word length should we include?
+max_word_len = 10;
 
 
 % CODE START %
 %%%%%%%%%%%%%%
 tic;
+if ~iscell(Files)
+    Files = {Files};
+end
 for ii=1:num_files
     fid = fopen(Files{ii});
     if fid == -1
@@ -61,14 +68,33 @@ for ii=1:num_files
     fclose(fid);
 end
 fprintf('%.2fs: finished parsing files\n', toc);
-
 [D.word,ii,ii] = unique(W{1}(:));
 D.word_count = accumarray(ii,1);
+D.word_len = zeros(size(D.word_count));
+for ii=1:length(D.word_len)
+    D.word_len(ii) = length(D.word{ii});
+end
 fprintf('%.2fs: finished creating and counting words\n', toc);
 
 [D.char,ii,ii] = unique(C);
 D.char = char(D.char);
 D.char_count = accumarray(ii,1);
+D.pos_count = cell(1,max_word_len);
+for ii=1:max_word_len
+    D.pos_count{ii} = zeros(length(D.char),ii);
+end
+for ii=1:size(W{1},1)
+    w = W{1}{ii};
+    len = length(w);
+    if len <= max_word_len
+        this_count = D.pos_count{len};
+        for jj=1:length(D.char)
+            ct = strfind(w, D.char(jj));
+            this_count(jj,ct) = this_count(jj,ct) + 1;
+        end
+        D.pos_count{len} = this_count;
+    end
+end
 fprintf('%.2fs: finished creating and counting chars\n', toc);
 
 num_chars = length(D.char);
@@ -103,6 +129,9 @@ fprintf('%.2fs: finished creating character n-gram matrices\n', toc);
 
 [D.char, idx] = setdiff(D.char, char(strip_chars)');
 D.char_count = D.char_count(idx);
+for ii=1:max_word_len
+    D.pos_count{ii} = D.pos_count{ii}(idx,:);
+end
 D.first_count = D.first_count(idx);
 D.first_bg_count = D.first_bg_count(idx,idx);
 D.char_bigram = D.char_bigram(idx,idx);
