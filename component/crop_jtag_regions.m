40f2efa204ac1d5684accf87d1c91c5a449934af
scottl <scottl>
2007-01-08 22:04:20 +0000

    added region number to the list of returned parameters.

--- component/crop_jtag_regions.m
+++ component/crop_jtag_regions.m
@@ -1,23 +1,27 @@
-function [new_img,pos] = crop_jtag_regions(jtag_file, img, keep_region_list, ...
-                         varargin)
+function [new_img,reg_num,pos] = crop_jtag_regions(jtag_file, img, ...
+                                 keep_region_list, varargin)
 % CROP_JTAG_REGIONS  Use a JTAG file to crop components due to their region type
 %
-% [CROPPED_IMG, REGIONS] = crop_jtag_regions(FILENAME, IN_IMG,JTAG_CROP_REGIONS)
+% [CROPPED_IMG, REG_NUM, REG_POS] = crop_jtag_regions(FILENAME, IN_IMG,
+%                                   JTAG_CROP_REGIONS, [VAR1, VAL1]...)
 %
 % This function looks for a JTAG named FILENAME, and if it finds one, it 
 % augments the components found in IN_IMG, removing those that are positioned 
 % outside regions belonging to the JTAG_CROP_REGIONS list.
 %
-% The updated component image is returned in CROPPED_IMG, and the list of 
-% region positions is returned in the nx4 matrix REGIONS.
+% The updated component image is returned in CROPPED_IMG, and the region
+% numbers in REG_NUM, and associated region positions in REG_POS
 %
 
 % CVS INFO %
 %%%%%%%%%%%%
-% $Id: crop_jtag_regions.m,v 1.3 2007-01-05 17:09:21 scottl Exp $
+% $Id: crop_jtag_regions.m,v 1.4 2007-01-08 22:04:20 scottl Exp $
 %
 % REVISION HISTORY
 % $Log: crop_jtag_regions.m,v $
+% Revision 1.4  2007-01-08 22:04:20  scottl
+% added region number to the list of returned parameters.
+%
 % Revision 1.3  2007-01-05 17:09:21  scottl
 % return region information also.
 %
@@ -46,7 +50,7 @@ end
 % and has the same name, but with the extension .jtag instead)
 new_img = img;
 if exist(jtag_file, 'file');
-    pos = jtag_region_finder(jtag_file, keep_region_list);
+    [pos,reg_num] = jtag_region_finder(jtag_file, keep_region_list);
     new_img(:) = bg_val;
     for ii=1:size(pos,1)
         new_img(pos(ii,2):pos(ii,4), pos(ii,1):pos(ii,3)) = ...
